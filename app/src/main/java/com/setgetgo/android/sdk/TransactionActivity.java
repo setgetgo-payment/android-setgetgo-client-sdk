589db35b767cab33fbf78557699788636ac33222
Andrea Brandi <anbrandi@LIVE.COM>
2018-04-21 16:53:42 +0100

    First commit

---
+++ app/src/main/java/com/setgetgo/android/sdk/TransactionActivity.java
@@ -0,0 +1,15 @@
+package com.setgetgo.android.sdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
