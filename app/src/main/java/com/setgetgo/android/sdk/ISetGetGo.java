feac6dda3e5d5a19716d105857fa0d5aa63372c6
Andrea Brandi <anbrandi@LIVE.COM>
2018-04-23 16:35:22 +0100

    added wraper class

--- app/src/main/java/com/setgetgo/android/sdk/ISetGetGo.java
+++ app/src/main/java/com/setgetgo/android/sdk/ISetGetGo.java
@@ -10,17 +10,17 @@ public interface ISetGetGo
      * @param payment The payment data.
      * @return The JSON representation of the payment response.
      */
-    JSONObject CreatePayment(Payment payment);
+    JSONObject CreatePayment(Payment payment) throws Exception;
 
     /**
      * @param paymentAddress The payment data.
      * @return The JSON representation of the payment status.
      */
-    JSONObject GetPayment(String paymentAddress);
+    JSONObject GetPayment(String paymentAddress) throws Exception;
 
     /**
      * @param currencyCode The currency code.
      * @return The JSON representation of the rate data..
      */
-    JSONObject GetRate(String currencyCode);
+    JSONObject GetRate(String currencyCode) throws Exception;
 }
